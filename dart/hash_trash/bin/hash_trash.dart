a1d7f81a011bd0a4c1a63e62426db9f9271695ad
IceArrow256 <icearrow256@gmail.com>
2021-02-25 14:20:01 +0200

    init dart project

---
+++ dart/hash_trash/bin/hash_trash.dart
@@ -0,0 +1,5 @@
+import 'package:hash_trash/hash_trash.dart' as hash_trash;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hash_trash.calculate()}!');
+}
---
+++ dart/hash_trash/test/hash_trash_test.dart
@@ -0,0 +1,8 @@
+import 'package:hash_trash/hash_trash.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
