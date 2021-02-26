fd1e7c4926fb2e608ba07b84a27cdefd8c16ec6e
Mykola Symon <simonnikolaj20@gmail.com>
2021-02-26 11:48:34 +0200

    prettify code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,19 @@
 #include <bitset>
-#include <cinttypes>
 #include <cmath>
+#include <cstdint>
+#include <exception>
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <stdexcept>
+#include <stdint.h>
 #include <string>
 #include <vector>
 
-static constexpr int ARR_LEN = 10;
-static constexpr int HASH_BITS = 256;   // 16384
-static constexpr int BASE_SIMPLE = 251; // 16381
-static constexpr int UP_POW = 20;
+static constexpr size_t ARR_LEN = 10;
+static constexpr size_t HASH_BITS = 256;
+static constexpr size_t BASE_SIMPLE = 251;
+static constexpr size_t UP_POW = 20;
 
 // get random uint64_t in [min, max)
 uint64_t rand_in_range(uint64_t min, uint64_t max) {
@@ -21,7 +24,7 @@ uint64_t rand_in_range(uint64_t min, uint64_t max) {
 }
 
 // print hihglight diff between two str
-std::string highlight_diff(std::string lhs, std::string rhs) {
+std::string highlight_diff(std::string const &lhs, std::string const &rhs) {
   std::string result;
   for (size_t i = 0; i < std::min(lhs.length(), rhs.length()); ++i) {
     result.append(lhs[i] == rhs[i] ? std::string("") + lhs[i]
@@ -29,17 +32,6 @@ std::string highlight_diff(std::string lhs, std::string rhs) {
                                          std::string("\033[0m"));
   }
   return result;
-}
-
-// randomization
-uint64_t *get_random_x(uint64_t len) {
-  uint64_t *arr = new uint64_t[len];
-
-  for (int i = 0; i < len; ++i) {
-    arr[i] = rand_in_range(0, static_cast<uint64_t>(pow(2, UP_POW)));
-  }
-
-  return arr;
 }
 
 // hash through exponential
@@ -54,50 +46,45 @@ uint64_t get_hash(uint64_t x) {
 int main() {
   // ========================================================================
 
-  uint64_t *X = get_random_x(ARR_LEN);
-  uint64_t *Y = new uint64_t[ARR_LEN];
-
-  std::copy(X, X + ARR_LEN, Y);
+  std::vector<uint64_t> X, Y;
   for (size_t i = 0; i < ARR_LEN; ++i) {
-    // changing bits of nums
-    // *(Y + i) = *(X + i) ^ (uint64_t)1 << rand_in_range(1, ARR_LEN);
-    *(Y + i) = *(X + i) ^ (uint64_t)1 << (uint64_t)i;
+    X.push_back(rand_in_range(0, static_cast<uint64_t>(pow(2, UP_POW))));
+    // Y.push_back(X[i] ^ (uint64_t)1 << rand_in_range(1, ARR_LEN));
+    Y.push_back(X[i] ^ (uint64_t)1 << (uint64_t)i);
   }
-
   // ========================================================================
 
-  std::vector<std::string> bins_X;
-  std::vector<std::string> bins_Y;
+  // vectors of nums binary representation
+  std::vector<std::string> bins_X, bins_Y;
 
-  std::vector<uint64_t> hash_X;
-  std::vector<uint64_t> hash_Y;
+  // vectors of hashes
+  std::vector<uint64_t> hash_X, hash_Y;
 
   for (size_t i = 0; i < ARR_LEN; ++i) {
-    bins_X.push_back(std::bitset<UP_POW>(*(X + i)).to_string());
-    hash_X.push_back(get_hash(*(X + i)));
-  }
-  for (size_t i = 0; i < ARR_LEN; ++i) {
-    bins_Y.push_back(std::bitset<UP_POW>(*(Y + i)).to_string());
-    hash_Y.push_back(get_hash(*(Y + i)));
+    bins_X.push_back(std::bitset<UP_POW>(X[i]).to_string());
+    bins_Y.push_back(std::bitset<UP_POW>(Y[i]).to_string());
+
+    hash_X.push_back(get_hash(X[i]));
+    hash_Y.push_back(get_hash(Y[i]));
   }
 
   // ========================================================================
 
   std::cout << "X:\n";
   for (size_t i = 0; i < ARR_LEN; ++i) {
-    std::cout << std::setw(20) << *(X + i) << "\t\t"
-              << highlight_diff(bins_X.at(i), bins_Y.at(i))
-              << "\t\tH(x): " << std::setw(10) << hash_X.at(i) << "\n";
+    std::cout << std::setw(20) << X[i] << "\t\t"
+              << highlight_diff(bins_X[i], bins_Y[i])
+              << "\t\tH(x): " << std::setw(10) << hash_X[i] << "\n";
   }
 
   // ========================================================================
 
   std::cout << "Y:\n";
   for (size_t i = 0; i < ARR_LEN; ++i) {
-    std::cout << std::setw(20) << *(Y + i) << "\t\t"
-              << highlight_diff(bins_Y.at(i), bins_X.at(i))
-              << "\t\tH(x): " << std::setw(10) << hash_Y.at(i) << "\t"
-              << std::boolalpha << (hash_Y.at(i) == hash_X.at(i)) << "\n";
+    std::cout << std::setw(20) << Y[i] << "\t\t"
+              << highlight_diff(bins_Y[i], bins_X[i])
+              << "\t\tH(x): " << std::setw(10) << hash_Y[i] << "\t"
+              << std::boolalpha << (hash_Y[i] == hash_X[i]) << "\n";
   }
   // ========================================================================
 
