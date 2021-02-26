c7b4170f5369b0afd0cb7a5e4ce6ba1d13c72312
IceArrow256 <icearrow256@gmail.com>
2021-02-26 10:19:11 +0200

    add more correlation calculation & add collision calculating

--- dart/hash_trash/bin/hash_trash.dart
+++ dart/hash_trash/bin/hash_trash.dart
@@ -1,11 +1,9 @@
 import 'package:ansicolor/ansicolor.dart';
 import 'package:hash_trash/hash_trash.dart';
-import 'package:scidart/numdart.dart';
-import 'package:scidart/scidart.dart';
 
 void main(List<String> arguments) {
   ansiColorDisabled = false;
-  var X = getRandomX(length: 10);
+  var X = getRandomX(length: 19);
   var Y = <int>[];
   for (var i = 0; i < X.length; i++) {
     Y.add(X[i] ^ 1 << i);
@@ -22,16 +20,39 @@ void main(List<String> arguments) {
     binsY.add(y.toRadixString(2).padLeft(20, '0'));
     hashY.add(getHash(value: y));
   }
+  var collisionsX = countCollisions(hashX);
+  var collisionsY = countCollisions(hashY);
+  var collisionBetweenXY = countCollisionBetween(hashX, hashY);
   print('X:');
   for (var i = 0; i < hashX.length; i++) {
-    print(
-        '${X[i].toString().padLeft(20, ' ')}\t\t${highlightDiff(left: binsX[i], right: binsY[i])}\t\tH(x)${hashX[i].toString().padLeft(20, ' ')}');
+    print(getOutputLine(
+        left: X[i],
+        binLeft: binsX[i],
+        binRight: binsY[i],
+        collisions: collisionsX,
+        hashLeft: hashX[i],
+        hashRight: hashY[i]));
   }
   print('Y:');
   for (var i = 0; i < hashY.length; i++) {
-    print(
-        '${Y[i].toString().padLeft(20, ' ')}\t\t${highlightDiff(left: binsY[i], right: binsX[i])}\t\tH(x)${hashY[i].toString().padLeft(20, ' ')}');
+    print(getOutputLine(
+        left: Y[i],
+        binLeft: binsY[i],
+        binRight: binsX[i],
+        collisions: collisionsY,
+        hashLeft: hashY[i],
+        hashRight: hashX[i]));
   }
-  print(
-      'Correlation coefficient: ${correlationCoefficient(x: hashX, y: hashY).toStringAsFixed(2)}');
+  print('Correlation coefficient (X, HASH X): ' +
+      correlationCoefficient(x: X, y: hashX).toStringAsFixed(2));
+  print('Correlation coefficient (Y, HASH Y): ' +
+      correlationCoefficient(x: Y, y: hashY).toStringAsFixed(2));
+  print('Correlation coefficient (HASH X, HASH Y): ' +
+      correlationCoefficient(x: hashX, y: hashY).toStringAsFixed(2));
+  print('Correlation coefficient (X, Y): ' +
+      correlationCoefficient(x: X, y: Y).toStringAsFixed(2));
+  print('Collision in X. Collision pairs: ${collisionsX.length}');
+  print('Collision in Y. Collision pairs: ${collisionsY.length}');
+  print('Collision between X and Y. Collision pairs: ' +
+      collisionBetweenXY.length.toString());
 }
--- dart/hash_trash/lib/hash_trash.dart
+++ dart/hash_trash/lib/hash_trash.dart
@@ -12,28 +12,6 @@ String bin({int value, int length}) {
   return result;
 }
 
-int getHash({@required int value, int hash_bits = 256, int base_simple = 251}) {
-  return (pow(base_simple, value).toInt()) % hash_bits;
-}
-
-List<int> getRandomX({@required int length, int exponent = 20}) {
-  final random = Random();
-  var x = <int>[];
-  for (var i = 0; i <= length; i++) {
-    x.add(random.nextInt(pow(2, exponent).toInt()));
-  }
-  return x;
-}
-
-String highlightDiff({@required String left, @required String right}) {
-  var result = '';
-  var pen = AnsiPen()..red();
-  for (var i = 0; i < min(left.length, right.length); i++) {
-    result += (left[i] == right[i]) ? left[i] : pen(left[i]);
-  }
-  return result;
-}
-
 double correlationCoefficient({List<int> x, List<int> y}) {
   var length = min(x.length, y.length);
   var avgX = 0.0;
@@ -61,4 +39,73 @@ double correlationCoefficient({List<int> x, List<int> y}) {
   var denominator = length * deltaX * deltaY;
   var corrCoef = numerator / denominator;
   return corrCoef;
+}
+
+List<int> countCollisionBetween(List<int> x, List<int> y) {
+  var collisions = <int>[];
+  for (var i = 0; i < min(x.length, y.length); i++) {
+    if (x[i] == y[i]) {
+      collisions.add(x[i]);
+    }
+  }
+  return collisions;
+}
+
+Map<int, int> countCollisions(List<int> x) {
+  var collisions = <int, int>{};
+  for (var i = 0; i < x.length - 1; i++) {
+    if (collisions.containsKey(x[i])) {
+      collisions[x[i]]++;
+    } else {
+      collisions[x[i]] = 1;
+    }
+  }
+  var new_collisions = <int, int>{};
+  for (var key in collisions.keys) {
+    if (collisions[key] > 1) {
+      new_collisions[key] = collisions[key];
+    }
+  }
+  return new_collisions;
+}
+
+int getHash({@required int value, int hash_bits = 256, int base_simple = 251}) {
+  return (pow(base_simple, value).toInt()) % hash_bits;
+}
+
+String getOutputLine(
+    {@required int left,
+    @required String binLeft,
+    @required String binRight,
+    @required Map<int, int> collisions,
+    @required int hashLeft,
+    @required int hashRight}) {
+  var pen = AnsiPen()..red();
+  var result = '${left.toString().padLeft(20, ' ')}\t\t';
+  result += '${highlightDiff(left: binLeft, right: binRight)}\t\t';
+  result += 'H(x)' +
+      (collisions.keys.contains(hashLeft)
+          ? pen(hashLeft.toString().padLeft(20, ' '))
+          : hashLeft.toString().padLeft(20, ' ')) +
+      '\t\t';
+  result += 'H(X) = H(Y) = ${hashLeft == hashRight}';
+  return result;
+}
+
+List<int> getRandomX({@required int length, int exponent = 20}) {
+  final random = Random();
+  var x = <int>[];
+  for (var i = 0; i <= length; i++) {
+    x.add(random.nextInt(pow(2, exponent).toInt()));
+  }
+  return x;
+}
+
+String highlightDiff({@required String left, @required String right}) {
+  var result = '';
+  var pen = AnsiPen()..red();
+  for (var i = 0; i < min(left.length, right.length); i++) {
+    result += (left[i] == right[i]) ? left[i] : pen(left[i]);
+  }
+  return result;
 }
