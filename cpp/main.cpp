bfe6b4a0eb437e66095ffcdc939ae706b6a3bb08
Mykola Symon <simonnikolaj20@gmail.com>
2021-02-26 13:00:28 +0200

    implement calc correlatio coeff

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,15 @@
+#include <algorithm>
 #include <bitset>
 #include <cmath>
 #include <cstdint>
-#include <exception>
 #include <iomanip>
 #include <iostream>
 #include <random>
-#include <stdexcept>
-#include <stdint.h>
 #include <string>
 #include <vector>
+#include <map>
 
-static constexpr size_t ARR_LEN = 10;
+static constexpr size_t ARR_LEN = 20;
 static constexpr size_t HASH_BITS = 256;
 static constexpr size_t BASE_SIMPLE = 251;
 static constexpr size_t UP_POW = 20;
@@ -43,14 +42,46 @@ uint64_t get_hash(uint64_t x) {
   return powered % HASH_BITS;
 }
 
+double get_correlatio_coeff(std::vector<uint64_t> const &lhs,
+                            std::vector<uint64_t> const &rhs) {
+  size_t length = std::min(lhs.size(), rhs.size());
+  double avg_x = .0, avg_y = .0, avg_x2 = .0, avg_y2 = .0;
+
+  for (size_t i = 0; i < length; ++i) {
+    avg_x += lhs[i];
+    avg_y += rhs[i];
+    avg_x2 += lhs[i] * lhs[i];
+    avg_y2 += rhs[i] * rhs[i];
+  }
+
+  avg_x /= length;
+  avg_y /= length;
+  avg_x2 /= length;
+  avg_y2 /= length;
+
+  double delta_x2 = avg_x2 - avg_x * avg_x;
+  double delta_y2 = avg_y2 - avg_y * avg_y;
+  double delta_x = std::sqrt(delta_x2);
+  double delta_y = std::sqrt(delta_y2);
+  double numerator = .0;
+
+  for (size_t i = 0; i < length; ++i) {
+    numerator += (lhs[i] - avg_x) * (rhs[i] - avg_y);
+  }
+
+  double denominator = length * delta_x * delta_y;
+
+  return numerator / denominator;
+}
+
 int main() {
   // ========================================================================
 
   std::vector<uint64_t> X, Y;
   for (size_t i = 0; i < ARR_LEN; ++i) {
     X.push_back(rand_in_range(0, static_cast<uint64_t>(pow(2, UP_POW))));
-    // Y.push_back(X[i] ^ (uint64_t)1 << rand_in_range(1, ARR_LEN));
-    Y.push_back(X[i] ^ (uint64_t)1 << (uint64_t)i);
+    Y.push_back(X[i] ^ (uint64_t)1 << rand_in_range(1, UP_POW - 1));
+    // Y.push_back(X[i] ^ (uint64_t)1 << (uint64_t)i);
   }
   // ========================================================================
 
@@ -83,9 +114,21 @@ int main() {
   for (size_t i = 0; i < ARR_LEN; ++i) {
     std::cout << std::setw(20) << Y[i] << "\t\t"
               << highlight_diff(bins_Y[i], bins_X[i])
-              << "\t\tH(x): " << std::setw(10) << hash_Y[i] << "\t"
-              << std::boolalpha << (hash_Y[i] == hash_X[i]) << "\n";
+              << "\t\tH(y): " << std::setw(10) << hash_Y[i]
+              << "\tH(X) = H(Y): " << std::boolalpha << (hash_Y[i] == hash_X[i])
+              << "\n";
   }
+  // ========================================================================
+
+  std::cout << "Correlation coefficient (X, H(X)):\t"
+            << get_correlatio_coeff(X, hash_X) << "\n";
+  std::cout << "Correlation coefficient (Y, H(Y)):\t"
+            << get_correlatio_coeff(Y, hash_Y) << "\n";
+  std::cout << "Correlation coefficient (H(X), H(Y)):\t"
+            << get_correlatio_coeff(hash_X, hash_Y) << "\n";
+  std::cout << "Correlation coefficient (X, Y):\t\t" << get_correlatio_coeff(X, Y)
+            << "\n";
+  
   // ========================================================================
 
   return 0;
