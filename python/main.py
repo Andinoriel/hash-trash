bdd169fcac0f653efd3d23264b706c42be6f1778
HanGerrel <volodimir.grigoryev@gmail.com>
2021-02-27 16:22:09 +0200

    fix names & improve output

--- python/main.py
+++ python/main.py
@@ -5,18 +5,21 @@ def get_hash(x: int, n: int, a: int = 251) -> int:
     return (a ** x) % n
 
 
-def get_random_x(length):
+def get_random_x(length, up_pow):
     x = []
-    for _ in range(length):
-        x.append(random.randrange(0, 2 ** 20))
-    return x
+    x_ = []
+    for i in range(length):
+        x.append(random.randrange(0, 2 ** up_pow))
+        x_.append(x[i] ^ 1 << random.randrange(0, up_pow))
+    return x, x_
 
 
-def correlation_coefficient(x: list, y: list, length: int) -> float:
+def correlation_coefficient(x: list, y: list) -> float:
     x_avg = 0
     y_avg = 0
     x2_avg = 0
     y2_avg = 0
+    length = min(len(x), len(y))
 
     for i in range(length):
         x_avg += x[i]
@@ -46,28 +49,49 @@ def correlation_coefficient(x: list, y: list, length: int) -> float:
     return corrcoef
 
 
+def get_bin(arr, up_pow):
+    bin_arr = []
+    for el in arr:
+        bin_arr.append(f"{el:0{up_pow}b}")
+    return bin_arr
+
+
+def get_differences(str_1, str_2) -> str:
+    result = ""
+    for i in range(min(len(str_1), len(str_2))):
+        result += str_1[i] if str_1[i] == str_2[i] else "\033[31m" + str_1[i] + "\033[0m"
+    return result
+
+
 def main():
     n = 2 ** 8
-    X = get_random_x(10)
-    Y = []
+    up_pow = 20
+    arr_x, arr_y = get_random_x(10, up_pow)
+    arr_x_hash = []
+    arr_y_hash = []
 
-    for x in X:
-        Y.append(get_hash(x, n))
+    for x in arr_x:
+        arr_x_hash.append(get_hash(x, n))
 
-    X_ = X
-    Y_ = []
-    for i in range(len(X)):
-        X_[i] = X[i] ^ 1 << i
+    for x in arr_y:
+        arr_y_hash.append(get_hash(x, n))
 
-    for x in X_:
-        Y_.append(get_hash(x, n))
+    arr_x_bin = get_bin(arr_x, up_pow)
+    arr_y_bin = get_bin(arr_y, up_pow)
 
-    print(f"x: {X}")
-    print(f"Y: {Y}")
-    print(f"x_: {X_}")
-    print(f"Y_: {Y_}")
+    print("X:")
+    for i in range(len(arr_x)):
+        print(f"{arr_x[i]:{up_pow}}\t\t{get_differences(arr_x_bin[i], arr_y_bin[i])}{arr_x_hash[i]:{up_pow}}")
+    print("Y:")
+    for i in range(len(arr_y)):
+        print(f"{arr_y[i]:{up_pow}}\t\t{get_differences(arr_y_bin[i], arr_x_bin[i])}{arr_y_hash[i]:{up_pow}}")
 
-    print(f"correlation_coefficient: {correlation_coefficient(Y, Y_, len(Y))}")
+    print(f"\ncorrelation_coefficient between x and H(x): {correlation_coefficient(arr_x, arr_x_hash)}")
+    print(f"correlation_coefficient between y and H(y): {correlation_coefficient(arr_y, arr_y_hash)}")
+    print(f"correlation_coefficient between x and y: {correlation_coefficient(arr_x, arr_y)}")
+    print(f"correlation_coefficient between H(x) and H(y): {correlation_coefficient(arr_x_hash, arr_y_hash)}")
+
+    return 0
 
 
 if __name__ == '__main__':
