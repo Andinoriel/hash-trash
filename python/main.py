0880f182f6ad1e74d7c1154af6767c782e50671b
HanGerrel <volodimir.grigoryev@gmail.com>
2021-02-27 17:37:38 +0200

    prettify code

--- python/main.py
+++ python/main.py
@@ -11,43 +11,8 @@ def get_random_x(length, up_pow):
     y = []
     for i in range(length):
         x.append(random.randrange(0, 2 ** up_pow))
-        y.append(x[i] ^ 1 << i)
+        y.append(x[i] ^ 1 << random.randrange(0, up_pow))
     return np.array(x), np.array(y)
-
-
-def correlation_coefficient(x: list, y: list) -> float:
-    x_avg = 0
-    y_avg = 0
-    x2_avg = 0
-    y2_avg = 0
-    length = min(len(x), len(y))
-
-    for i in range(length):
-        x_avg += x[i]
-        y_avg += y[i]
-        x2_avg += x[i] ** 2
-        y2_avg += y[i] ** 2
-
-    x_avg /= length
-    y_avg /= length
-    x2_avg /= length
-    y2_avg /= length
-
-    delta_x2 = x2_avg - x_avg ** 2
-    delta_y2 = y2_avg - y_avg ** 2
-    delta_x = delta_x2 ** (1 / 2)
-    delta_y = delta_y2 ** (1 / 2)
-
-    numerator = 0
-
-    for i in range(length):
-        numerator += (x[i] - x_avg) * (y[i] - y_avg)
-
-    denominator = length * delta_x * delta_y
-
-    corrcoef = numerator / denominator
-
-    return corrcoef
 
 
 def get_bin(arr, up_pow):
@@ -87,10 +52,10 @@ def main():
     for i in range(len(arr_y)):
         print(f"{arr_y[i]:{up_pow}}\t\t{get_differences(arr_y_bin[i], arr_x_bin[i])}{arr_y_hash[i]:{up_pow}}")
 
-    print(f"\ncorrelation_coefficient between x and H(x): {np.corrcoef(arr_x, arr_x_hash)}")
-    print(f"correlation_coefficient between y and H(y): {np.corrcoef(arr_y, arr_y_hash)}")
-    print(f"correlation_coefficient between x and y: {np.corrcoef(arr_x, arr_y)}")
-    print(f"correlation_coefficient between H(x) and H(y): {np.corrcoef(arr_x_hash, arr_y_hash)}")
+    print(f"\ncorrelation_coefficient between x and H(x): {round(np.corrcoef(arr_x, arr_x_hash)[1][0], 4)}")
+    print(f"correlation_coefficient between y and H(y): {round(np.corrcoef(arr_y, arr_y_hash)[1][0], 4)}")
+    print(f"correlation_coefficient between x and y: {round(np.corrcoef(arr_x, arr_y)[1][0], 4)}")
+    print(f"correlation_coefficient between H(x) and H(y): {round(np.corrcoef(arr_x_hash, arr_y_hash)[1][0], 4)}")
 
     return 0
 
