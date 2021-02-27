075b7276d97e1ba96059a369704a1e67f6567f96
Mykola Symon <simonnikolaj20@gmail.com>
2021-02-27 14:24:42 +0200

    implement calc correlation coefficient

--- rust/hash-trash/src/main.rs
+++ rust/hash-trash/src/main.rs
@@ -31,6 +31,40 @@ fn highlight_diff(lhs: &String, rhs: &String) -> String {
     result
 }
 
+fn get_correlatio_coeff(lhs: &Vec<u64>, rhs: &Vec<u64>) -> f64 {
+    let length = min(lhs.len(), rhs.len());
+    let mut avg_x = 0.;
+    let mut avg_y = 0.;
+    let mut avg_x2 = 0.;
+    let mut avg_y2 = 0.;
+
+    for i in 0..length {
+        avg_x += lhs[i] as f64;
+        avg_y += rhs[i] as f64;
+        avg_x2 += (lhs[i] * lhs[i]) as f64;
+        avg_y2 += (rhs[i] * rhs[i]) as f64;
+    }
+
+    avg_x /= length as f64;
+    avg_y /= length as f64;
+    avg_x2 /= length as f64;
+    avg_y2 /= length as f64;
+
+    let delta_x2 = avg_x2 - avg_x * avg_x;
+    let delta_y2 = avg_y2 - avg_y * avg_y;
+    let delta_x = delta_x2.sqrt();
+    let delta_y = delta_y2.sqrt();
+    let mut numerator = 0.;
+
+    for i in 0..length {
+        numerator += (lhs[i] as f64 - avg_x) * (rhs[i] as f64 - avg_y);
+    }
+
+    let denominator = length as f64 * delta_x * delta_y;
+
+    numerator / denominator
+}
+
 fn main() {
     // ====================================================
 
@@ -40,7 +74,7 @@ fn main() {
     let mut rng = rand::thread_rng();
     for i in 0..ARR_LEN {
         x.push(rng.gen_range(0..u64::pow(2, UP_POW)));
-        y.push(x[i] ^ 1 << rng.gen_range(1..10));
+        y.push(x[i] ^ 1 << rng.gen_range(1..UP_POW));
     }
 
     // ====================================================
@@ -81,4 +115,23 @@ fn main() {
             hash_x[i] == hash_y[i]
         );
     }
+
+    // ====================================================
+
+    println!(
+        "Correlation coefficient (X, H(X)):\t{}",
+        get_correlatio_coeff(&x, &hash_x)
+    );
+    println!(
+        "Correlation coefficient (Y, H(Y)):\t{}",
+        get_correlatio_coeff(&y, &hash_y)
+    );
+    println!(
+        "Correlation coefficient (H(X), H(Y)):\t{}",
+        get_correlatio_coeff(&hash_x, &hash_y)
+    );
+    println!(
+        "Correlation coefficient (X, Y):\t{}",
+        get_correlatio_coeff(&x, &y)
+    );
 }
